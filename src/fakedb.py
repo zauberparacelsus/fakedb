67f019807acd2513e1638896ef1d52880c299c32
Zauber Paracelsus <zauber@gridmail.org>
2017-05-26 17:21:27 -0400

    File Locking and Exception Handling
    Implemented file locking to prevent multiple processes from trying to read or write the same file at once (probably won't work on Windows), and added some extra exception handling.

--- src/fakedb.py
+++ src/fakedb.py
@@ -3,13 +3,18 @@
 import os, types, uuid
 import json, bson, hjson
 
+from fcntl import flock, LOCK_EX, LOCK_NB, LOCK_UN
+
+
 class FakeDB:
 	datadir = ""
 	readflag = "r"
 	writeflag = "w"
 	fileformat = ".json"
+	test = 0
 	codec = None
 	def __init__(self, directory="./data", jsonformat="json"):
+		self.test = 0
 		self.datadir = os.path.abspath(directory)
 		if(jsonformat == "json"):
 			self.codec = json
@@ -27,41 +32,76 @@ class FakeDB:
 			name += self.fileformat
 		outfile = os.path.join(self.datadir, name)
 		
-		if(os.path.isfile(outfile)):
+		#if(os.path.isfile(outfile)):
+		try:
 			with open(outfile, self.readflag) as f:
-				oldfile = self.codec.loads(f.read())
 				try:
-					if(body['revision'] != oldfile['revision']):
-						return ""
-				except KeyError:
-					return ""
+					flock(f, LOCK_EX | LOCK_NB)
+				except:
+					return None
+				ignoreRev = False
+				try:
+					oldfile = self.codec.loads(f.read())
+				except self.codec.decoder.JSONDecodeError:
+					ignoreRev = True
+				
+				if(ignoreRev == False):
+					try:
+						if(body['revision'] != oldfile['revision']):
+							flock(f, LOCK_UN)
+							return None
+					except KeyError:
+						flock(f, LOCK_UN)
+						pass
+				flock(f, LOCK_UN)
+		except IOError:
+			pass
 		
 		body["revision"] = uuid.uuid4().hex
 		
-		os.makedirs(os.path.dirname(outfile), exist_ok=True)
-		with open(outfile, self.writeflag) as f:
-			f.write(self.codec.dumps(body))
+		if not os.path.exists(os.path.dirname(outfile)):
+			os.makedirs(os.path.dirname(outfile))
+		try:
+			with open(outfile, self.writeflag) as f:
+				try:
+					flock(f, LOCK_EX | LOCK_NB)
+				except BlockingIOError:
+					return None
+				#print(">> " + self.codec.dumps(body))
+				f.write(self.codec.dumps(body))
+				flock(f, LOCK_UN)
+		except IOError:
+			return None
 		return body["revision"]
 	
 	def read(self, name=""):
 		if not name.endswith(self.fileformat):
 			name += self.fileformat
 		infile = os.path.join(self.datadir, name)
+		ret = None
 		
-		with open(infile, self.readflag) as f:
-			if(os.path.isfile(infile)):
-				return self.codec.loads(f.read())
-			else:
-				return ""
+		try:
+			with open(infile, self.readflag) as f:
+				data = f.read()
+				try:
+					ret = self.codec.loads(data)
+				except self.codec.decoder.JSONDecodeError:
+					pass
+		except IOError:
+			pass
+		return ret
 
 
 if __name__ == "__main__":
-	a = FakeDB(jsonformat="json")
+	a = FakeDB(jsonformat="hjson")
 	
-	testdoc = {"a":1, "b":2, "c":3}
+	testdoc = a.read("test")
+	if(testdoc == None):
+		testdoc = {"a":1, "b":2, "c":3}
 	a.write(testdoc, "test")
 	
 	testdoc = a.read("test")
+	
 	testdoc["a"] += 1
 	testdoc["b"] += 2
 	testdoc["c"] += 3
